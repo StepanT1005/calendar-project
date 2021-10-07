7b97ca9f7bc2b55950a73d39be8586c10cfb6d66
StepanT1005 <stkdqpuu@gmail.com>
2021-10-07 14:51:22 +0300

    123

--- DOM/task6/index.js
+++ DOM/task6/index.js
@@ -1,6 +1,6 @@
 export function setButton(buttonText) {
 const body = document.querySelector('body');
 body.innerHTML = `<button>${buttonText}</button}`;
-body.textContent =`<button>${buttonText}</button}`;
 }
 
+setButton('text')
